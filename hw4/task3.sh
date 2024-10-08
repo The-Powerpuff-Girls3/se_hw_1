0bea0c0d5663dbaa37146a58596e5548a090c193
Hua Yang <yanghuattt@gmail.com>
2024-10-08 09:25:54 -0400

    fix task3 to a single line

--- hw4/task3.sh
+++ hw4/task3.sh
@@ -1,8 +1,10 @@
 # remove "\r"
-tr -d '\r' < titanic.csv > titanic_cleaned.csv
 # step1--Extract passengers from 2nd class who embarked at Southampton.
-awk -F',' '$3 == 2' titanic_cleaned.csv | grep ',S$' > titanic_filtered.csv
+#tr -d '\r' < titanic.csv | awk -F',' '$3 == 2' | grep ',S$' > titanic_filtered.csv
 # step2--Then replace male/female labels with respectively M/F.
-sed 's/female/F/g; s/male/M/g' titanic_filtered.csv > titanic_replaced.csv
+#sed 's/female/F/g; s/male/M/g' titanic_filtered.csv > titanic_replaced.csv
 # step3--Finally, calculate the average age of the filtered passengers.
-awk -F',' '{if ($7 != "") sum += $7; count += ($7 != "")} END {if (count > 0) print "average age:", sum/count, "among", count, "passengers who have age info"; else print "No valid ages"}' titanic_replaced.csv
+#awk -F',' '{if ($7 != "") sum += $7; count += ($7 != "")} END {if (count > 0) print "average age:", sum/count, "among", count, "passengers who have age info"; else print "No valid ages"}' titanic_replaced.csv
+
+# a single pipeline of commands for all the tasks a, b, and c
+tr -d '\r' < titanic.csv | awk -F',' '$3 == 2' | grep ',S$' | sed 's/female/F/g; s/male/M/g' | awk -F',' '{if ($7 != "") sum += $7; count += ($7 != "")} END {if (count > 0) print "average age:", sum/count, "among", count, "passengers who have age info"; else print "No valid ages"}' 
