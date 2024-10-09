11155aec40a5da3b8fb14d942d22ec5bfa6aef21
YinanWusoymilk <122840322+YinanWusoymilk@users.noreply.github.com>
2024-10-09 15:30:34 +0000

    use ps command to kill infinite.sh

--- hw4/task1.sh
+++ hw4/task1.sh
@@ -1,1 +1,11 @@
 #task1.sh 
+
+# 找到 infinite.sh 进程并终止
+PID=$(ps aux | grep '[i]nfinite.sh' | awk '{print $2}')
+
+if [ -n "$PID" ]; then
+    kill -9 $PID
+    echo "Process infinite.sh (PID: $PID) has been killed."
+else
+    echo "No infinite.sh process found."
+fi
