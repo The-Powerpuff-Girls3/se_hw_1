6d602b3f35d9cdb9a2b7f1bafc7133ab82118538
lvxuntao <93198782+lvxuntao@users.noreply.github.com>
2024-10-23 01:13:18 +0000

    task2 fix

--- hw4/dataset1/task2.sh
+++ hw4/dataset1/task2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 grep -l "sample" file* | xargs grep -o "CSC510" | uniq -c | grep -E '^[ ]*([3-9]|[0-9]{2,}) ' | cut -d: -f1 | \
-sort | gawk '{print $2}' | xargs ls -l | sort -k5,5nr | gawk '{print $9}' | \
+gawk '{print $1, $2}' | xargs -I {} sh -c 'count=$(echo "{}" | awk "{print \$1}"); file=$(echo "{}" | awk "{print \$2}"); size=$(stat -c%s "$file"); echo "$count $file $size"' | sort -k1,1nr -k3,3n | gawk '{print $2}' | \
 sed 's/file_/filtered_/'
